ff06d0c74a6e36703db638d25915d942acb1a366
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-12-05 21:46:04 +0200

    feat: add toastify, function to display notification and test

---
+++ src/utils/displayNotification/index.ts
@@ -0,0 +1,21 @@
+import Toastify from 'toastify-js';
+
+import './displayNotification.scss';
+
+export default function displayNotification(
+  text: string,
+  type: 'info' | 'success' | 'error',
+  duration = 5000,
+  onClick?: () => void
+) {
+  Toastify({
+    text,
+    duration,
+    close: true,
+    gravity: 'top',
+    position: 'right',
+    stopOnFocus: true,
+    onClick,
+    className: `toastify-${type}`,
+  }).showToast();
+}
