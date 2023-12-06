d5968f880f66faf879694254b62893c777a51a71
IrinaEnotova <irinkayashina@gmail.com>
2023-12-06 05:15:23 +0400

    feat: add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,50 @@
+import styles from './Footer.module.scss';
+
+export default function Footer() {
+  return (
+    <div className={styles.wrapper}>
+      <ul className={styles['github-list']}>
+        <a
+          className={styles.link}
+          href="https://github.com/Howl404"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src="/github-mark.svg" alt="icon" width={15} height={15} />
+          <li>Arturas Viachirevas</li>
+        </a>
+        <a
+          className={styles.link}
+          href="https://github.com/Wystov"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src="/github-mark.svg" alt="icon" width={15} height={15} />
+          <li>Kostiantyn Suslov</li>
+        </a>
+        <a
+          className={styles.link}
+          href="https://github.com/IrinaEnotova"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src="/github-mark.svg" alt="icon" width={15} height={15} />
+          <li>Irina Akhanteva</li>
+        </a>
+      </ul>
+      <ul className={styles['rs-list']}>
+        <li>2023</li>
+        <a
+          className={styles.link}
+          href="https://rs.school/react/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <li>
+            <img src="/rs-school.svg" alt="RS School" width={70} height={30} />
+          </li>
+        </a>
+      </ul>
+    </div>
+  );
+}
