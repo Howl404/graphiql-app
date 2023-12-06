31397f31615eded9ab78cefb736e73d598e039d7
IrinaEnotova <irinkayashina@gmail.com>
2023-12-06 05:18:07 +0400

    refactor: update layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,14 +1,18 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import Footer from '../Footer';
+import Header from '../Header';
+
 export default function Layout() {
   return (
     <>
-      <h2>HEADER</h2>
-      <Suspense fallback={<h1>Loading...</h1>}>
-        <Outlet />
-      </Suspense>
-      <h2>FOOTER</h2>
+      <Header>
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <Outlet />
+        </Suspense>
+      </Header>
+      <Footer />
     </>
   );
 }
