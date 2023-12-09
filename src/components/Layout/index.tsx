c579e645ed8f10937a5c8f24e497d83362c24d3b
IrinaEnotova <irinkayashina@gmail.com>
2023-12-09 16:54:07 +0400

    fix: add import ErrorBoundary

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import ErrorBoundary from 'components/ErrorBoundary';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
 import Loader from 'components/UI/Loader';
