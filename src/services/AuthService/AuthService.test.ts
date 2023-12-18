c05124dbce7475ca823dfb4830245b6e54b70b4c
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-12-18 16:05:47 +0200

    feat: tests for auth service and auth error handler

---
+++ src/services/AuthService/AuthService.test.ts
@@ -0,0 +1,138 @@
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
+import { addDoc, collection, getDocs, query } from 'firebase/firestore';
+import { describe, expect, it, Mock, vi } from 'vitest';
+
+import AuthService from 'services/AuthService';
+
+vi.mock('firebase/auth', () => ({
+  signInWithEmailAndPassword: vi.fn(),
+  createUserWithEmailAndPassword: vi.fn(),
+  signOut: vi.fn(),
+  getAuth: vi.fn(),
+  signInWithPopup: vi.fn(),
+  GoogleAuthProvider: vi.fn(),
+}));
+
+vi.mock('firebase/firestore', () => ({
+  getFirestore: vi.fn(),
+  addDoc: vi.fn(),
+  collection: vi.fn(),
+  getDocs: vi.fn(),
+  query: vi.fn(),
+  where: vi.fn(),
+}));
+
+vi.mock('src/firebase.ts', () => ({
+  auth: 'Auth object',
+  db: 'Database object',
+}));
+
+vi.mock('services/authErrorHandler.ts', () => ({
+  authErrorHandler: vi.fn(),
+}));
+
+describe('Auth Service methods successful requests returns true', () => {
+  it('Should return true after successful sign in with Google', async () => {
+    vi.mocked(GoogleAuthProvider).mockImplementation(
+      () => ({}) as GoogleAuthProvider
+    );
+    vi.mocked(signInWithPopup as Mock).mockResolvedValue({
+      user: { uid: '123', email: 'user@example.com' },
+    });
+    vi.mocked(query as Mock).mockImplementation(() => ({}));
+    vi.mocked(getDocs as Mock).mockResolvedValue({ docs: [] });
+    vi.mocked(addDoc as Mock).mockResolvedValue({});
+    vi.mocked(collection as Mock).mockReturnValue({});
+
+    const result = await AuthService.signInWithGoogle();
+    expect(result).toBe(true);
+  });
+
+  it('Should return true after successful log in with email and password', async () => {
+    vi.mocked(signInWithEmailAndPassword as Mock).mockImplementationOnce(() =>
+      Promise.resolve({ user: { uid: '123', email: 'test@example.com' } })
+    );
+
+    const result = await AuthService.logInWithEmailAndPassword(
+      'test@example.com',
+      'password123'
+    );
+    expect(result).toBe(true);
+  });
+
+  it('Should return true after successful sign up with email and password', async () => {
+    vi.mocked(createUserWithEmailAndPassword as Mock).mockImplementationOnce(
+      () => Promise.resolve({ user: { uid: '123', email: 'test@example.com' } })
+    );
+    vi.mocked(collection as Mock).mockReturnValue({});
+    vi.mocked(addDoc as Mock).mockResolvedValue({});
+
+    const result = await AuthService.registerWithEmailAndPassword(
+      'test@example.com',
+      'password123'
+    );
+    expect(result).toBe(true);
+  });
+
+  it('Should call sign out method with auth object', async () => {
+    vi.mocked(signOut as Mock).mockImplementationOnce(() =>
+      Promise.resolve({ user: { uid: '123', email: 'test@example.com' } })
+    );
+
+    await AuthService.signOutUser();
+
+    expect(signOut as Mock).toHaveBeenCalledWith('Auth object');
+  });
+});
+
+describe('If something go wrong, Auth Service methods calls authErrorHandler', () => {
+  it('Should return false and call authErrorHandler after unsuccessful sign in with Google', async () => {
+    vi.mocked(GoogleAuthProvider).mockImplementation(
+      () => ({}) as GoogleAuthProvider
+    );
+    vi.mocked(signInWithPopup as Mock).mockResolvedValue({
+      user: { uid: '123', email: 'user@example.com' },
+    });
+    vi.mocked(query as Mock).mockImplementation(() => {
+      throw new Error('Google test error');
+    });
+    vi.mocked(getDocs as Mock).mockResolvedValue({ docs: [] });
+    vi.mocked(addDoc as Mock).mockResolvedValue({});
+    vi.mocked(collection as Mock).mockReturnValue({});
+
+    const result = await AuthService.signInWithGoogle();
+    expect(result).toBe(false);
+  });
+
+  it('Should return false and call authErrorHandler after unsuccessful log in', async () => {
+    vi.mocked(createUserWithEmailAndPassword as Mock).mockImplementationOnce(
+      () => {
+        throw new Error('Test error');
+      }
+    );
+
+    const result = await AuthService.registerWithEmailAndPassword(
+      'test@example.com',
+      'password123'
+    );
+    expect(result).toBe(false);
+  });
+
+  it('Should return false and call authErrorHandler after unsuccessful log in', async () => {
+    vi.mocked(signInWithEmailAndPassword as Mock).mockImplementationOnce(() => {
+      throw new Error('Test error');
+    });
+
+    const result = await AuthService.logInWithEmailAndPassword(
+      'test@example.com',
+      'password123'
+    );
+    expect(result).toBe(false);
+  });
+});
