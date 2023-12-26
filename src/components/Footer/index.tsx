fbd4e0c242d50bbab40f0f36563e0f0510715260
IrinaEnotova <irinkayashina@gmail.com>
2023-12-26 21:18:20 +0400

    feat: add theme for common components

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,21 +1,39 @@
+import { useContext } from 'react';
+
+import Themes from 'enums/themes';
+
+import cls from 'utils/classnames';
+
+import { AppThemeContext } from 'context/ThemeContext';
+
 import githubIcon from 'assets/github-mark.svg';
 import rsschoolIcon from 'assets/rs-school.svg';
 
 import styles from './Footer.module.scss';
 
 export default function Footer() {
+  const { themeType } = useContext(AppThemeContext);
+
   const developers = [
     { name: 'Arturas Viachirevas', github: 'https://github.com/Howl404' },
     { name: 'Kostiantyn Suslov', github: 'https://github.com/Wystov' },
     { name: 'Irina Akhanteva', github: 'https://github.com/IrinaEnotova' },
   ];
   return (
-    <div className={styles.wrapper}>
+    <div
+      className={cls(
+        styles.wrapper,
+        themeType === Themes.Dark ? styles.wrapperDark : styles.wrapperLight
+      )}
+    >
       <ul className={styles.githubList}>
         {developers.map((developer) => (
           <li key={developer.github} data-testid="developer-item">
             <a
-              className={styles.link}
+              className={cls(
+                styles.link,
+                themeType === Themes.Dark ? styles.linkDark : styles.linkLight
+              )}
               href={developer.github}
               target="_blank"
               rel="noreferrer"
