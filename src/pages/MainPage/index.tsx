331658ca1e918f230e1418782ea24f500c6f646c
Howl404 <114860794+Howl404@users.noreply.github.com>
2024-01-02 00:59:39 +0200

    feat: main page test, font size

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -7,26 +7,51 @@ import Paths from 'enums/paths';
 
 import useTranslation from 'hooks/useTranslation';
 
+import Loader from 'components/UI/Loader';
+
 import styles from './MainPage.module.scss';
 
 export default function MainPage() {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
 
   const translation = useTranslation();
+
+  if (loading) {
+    return (
+      <div className={styles.loaderContainer}>
+        <Loader />
+      </div>
+    );
+  }
 
   return (
     <div>
       <div className={styles.buttonsContainer}>
         {user ? (
-          <Button component={Link} to={Paths.Graphiql} variant={'outlined'}>
+          <Button
+            component={Link}
+            to={Paths.Graphiql}
+            variant={'outlined'}
+            data-testid="mainpage"
+          >
             {translation(`Shared.MainPage`)}
           </Button>
         ) : (
           <>
-            <Button component={Link} to={Paths.Auth} variant={'outlined'}>
+            <Button
+              component={Link}
+              to={Paths.Auth}
+              variant={'outlined'}
+              data-testid="signin"
+            >
               {translation(`Shared.SignIn`)}
             </Button>
-            <Button component={Link} to={Paths.Auth} variant={'outlined'}>
+            <Button
+              component={Link}
+              to={Paths.Auth}
+              variant={'outlined'}
+              data-testid="signup"
+            >
               {translation(`Shared.SignUp`)}
             </Button>
           </>
