16d641c354d4e1d4d8ff95f60cefdec88c1f18cb
Howl404 <114860794+Howl404@users.noreply.github.com>
2024-01-07 10:56:27 +0200

    feat: console log to see full error

--- src/services/AuthService/authErrorHandler.ts
+++ src/services/AuthService/authErrorHandler.ts
@@ -21,6 +21,8 @@ export default function authErrorHandler(
         : error.code,
       'error'
     );
+
+    console.log(error);
   } else {
     displayNotification(translation('AuthPage.auth/unknown'), 'error');
   }
